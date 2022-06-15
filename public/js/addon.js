26f76a7bf67be90683869746cc7c154e9f5776b3
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-06-15 15:09:19 +0700

    try out getting context

--- public/js/addon.js
+++ public/js/addon.js
@@ -6,9 +6,13 @@ function getInfo() {
 }
 
 function checkAPI() {
-    AP.request('/rest/api/2/issue/DW-1/changelog', {
-        success: function(res) {
-            console.log(res);
-        }
+    
+    AP.context.getContext(function(response){
+        console.log(response);
     });
+    // AP.request('/rest/api/2/issue/DW-1/changelog', {
+    //     success: function(res) {
+    //         console.log(res);
+    //     }
+    // });
 }

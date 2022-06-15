4d0533800e0d328ba79296f3fcb8d7ae6e4e0972
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-06-15 14:42:46 +0700

    check history API

--- public/js/addon.js
+++ public/js/addon.js
@@ -1,1 +1,14 @@
 /* App frontend script */
+function getInfo() {
+    debugger;
+    console.log("something");
+    debugger;
+}
+
+function checkAPI() {
+    AP.request('/rest/api/2/issue/DW-1/changelog', {
+        success: function(res) {
+            console.log(res);
+        }
+    });
+}

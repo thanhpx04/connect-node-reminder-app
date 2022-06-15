053017dcc774d6909376c040b29aae7c64bf3119
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-06-15 15:51:19 +0700

    draft

--- public/js/addon.js
+++ public/js/addon.js
@@ -6,6 +6,13 @@ function getInfo() {
 }
 
 function checkAPI() {
+
+    AP.context.getContext().then(response => {
+        console.log(
+          `Response: ${response.jira.issue.key}`
+        );
+        return response.text();
+    });
     
     AP.context.getContext(function(response){
         console.log(response.jira.issue.key);
@@ -15,4 +22,21 @@ function checkAPI() {
     //         console.log(res);
     //     }
     // });
+fetch('https://your-domain.atlassian.net/rest/api/2/issue/{issueIdOrKey}/changelog', {
+  method: 'GET',
+  headers: {
+    'Authorization': `Basic ${Buffer.from(
+      'email@example.com:<api_token>'
+    ).toString('base64')}`,
+    'Accept': 'application/json'
+  }
+})
+  .then(response => {
+    console.log(
+      `Response: ${response.status} ${response.statusText}`
+    );
+    return response.text();
+  })
+  .then(text => console.log(text))
+  .catch(err => console.error(err));
 }

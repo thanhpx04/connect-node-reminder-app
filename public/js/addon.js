0c830c04499a5b3b02d43fc453439a8facfaefd8
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-06-15 16:25:34 +0700

    finish draft

--- public/js/addon.js
+++ public/js/addon.js
@@ -7,36 +7,21 @@ function getInfo() {
 
 function checkAPI() {
 
-    AP.context.getContext().then(response => {
-        console.log(
-          `Response: ${response.jira.issue.key}`
-        );
-        return response.text();
-    });
+    AP.context.getContext().then(
+        response => {
+            AP.request(`/rest/api/2/issue/${response.jira.issue.key}/changelog`).then(
+                res => {
+                    console.log(res);
+                });
+        }
+    );
     
-    AP.context.getContext(function(response){
-        console.log(response.jira.issue.key);
-    });
+    // AP.context.getContext(function(response){
+    //     console.log(response.jira.issue.key);
+    // });
     // AP.request('/rest/api/2/issue/DW-1/changelog', {
     //     success: function(res) {
     //         console.log(res);
     //     }
     // });
-fetch('https://your-domain.atlassian.net/rest/api/2/issue/{issueIdOrKey}/changelog', {
-  method: 'GET',
-  headers: {
-    'Authorization': `Basic ${Buffer.from(
-      'email@example.com:<api_token>'
-    ).toString('base64')}`,
-    'Accept': 'application/json'
-  }
-})
-  .then(response => {
-    console.log(
-      `Response: ${response.status} ${response.statusText}`
-    );
-    return response.text();
-  })
-  .then(text => console.log(text))
-  .catch(err => console.error(err));
 }

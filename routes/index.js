7edfdfc63aeaa8c206ff6f5ee3760aeb0b2e8546
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-06-15 10:53:59 +0700

    init app UI

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ export default function routes(app, addon) {
 
     // This is an example route used by "generalPages" module (see atlassian-connect.json).
     // Verify that the incoming request is authenticated with Atlassian Connect.
-    app.get('/hello-world', addon.authenticate(), (req, res) => {
+    app.get('/main', (req, res) => {
         // Rendering a template is easy; the render method takes two params: the name of the component or template file, and its props.
         // Handlebars and jsx are both supported, but please note that jsx changes require `npm run watch-jsx` in order to be picked up by the server.
         res.render(

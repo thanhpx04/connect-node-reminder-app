cce8e634be1dbe33b9c9f40a02cbad088029e0f8
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-06-20 17:42:19 +0700

    draft

--- public/js/addon.js
+++ public/js/addon.js
@@ -8,24 +8,15 @@ function checkAPI() {
             issueKey = response.jira.issue.key;
             if(issueKey){
                 AP.request(`/rest/api/2/issue/${issueKey}/changelog`)
-                .then(response => {
+                .then(res => {
+                    let resObj, listHistoryItem, listHistoryStatus;
                     debugger;
-                    console.log(
-                      `Response: ${response.status} ${response.statusText}`
-                    );
-                    return response.text();
-                  })
-                  .then(text => console.log(text))
-                  .catch(err => console.error(err));
-                // .then((err, res, body) => {
-                //     let resObj, listHistoryItem, listHistoryStatus;
-                //     debugger;
-                //     resObj = JSON.parse(res);
-                //     listHistoryItem = resObj.values;
-                //     listHistoryStatus = listHistoryItem.filter(findStatusItemOfHistory);
-                //     console.log(listHistoryStatus);
-                // })
-                // .catch(err => console.log('Request Failed', err));
+                    resObj = JSON.parse(res.body);
+                    listHistoryItem = resObj.values;
+                    listHistoryStatus = listHistoryItem.filter(findStatusItemOfHistory);
+                    console.log(listHistoryStatus);
+                })
+                .catch(err => console.log('Request Failed', err));
             }
         }
     );

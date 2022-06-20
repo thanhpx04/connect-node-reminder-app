df4d367b03912a3e09497e4ce0308b48695ba405
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-06-20 17:00:54 +0700

    change response code

--- public/js/addon.js
+++ public/js/addon.js
@@ -8,20 +8,15 @@ function checkAPI() {
             issueKey = response.jira.issue.key;
             if(issueKey){
                 AP.request(`/rest/api/2/issue/${issueKey}/changelog`)
-                .then(response => response.json())
-                .then(json => console.log(json))
+                .then(res => {
+                    let resObj, listHistoryItem, listHistoryStatus;
+                    debugger;
+                    resObj = JSON.parse(res);
+                    listHistoryItem = res.values;
+                    listHistoryStatus = listHistoryItem.filter(findStatusItemOfHistory);
+                    console.log(listHistoryStatus);
+                })
                 .catch(err => console.log('Request Failed', err));
-                // then(
-                //     res => {
-                //         let listHistoryItem, listHistoryStatus;
-
-                //         debugger;
-                //         listHistoryItem = res.values;
-                //         listHistoryStatus = listHistoryItem.filter(findStatusItemOfHistory);
-                //         console.log(listHistoryStatus);
-                //     }
-                // );
-
             }
         }
     );
